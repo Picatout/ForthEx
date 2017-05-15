45f04361e29733c490f006dd4bd491751e2683fe
picatout <jd_temp@yahoo.fr>
2017-05-15 00:02:39 -0400

    Travail sur docgen.c

--- docgen.c
+++ docgen.c
@@ -19,9 +19,122 @@
 
 static char* path="ForthEx.X/";
 static char* ext=".s";
+static char html[1024];
 
+int scan(const char *text, char c,int from){
+	while (text[from] && text[from]!=c){
+		from++;
+	}
+	return from;
+}
+
+int skip(const char *text, char c,int from){
+	while (text[from] && text[from]==c){
+		from++;
+    }
+	return from;
+}
+
+int word(char *text){
+	int i,j;
+	i=skip(text,' ',0);
+	j=0;
+	while (text[i] && text[i]!=' '){
+		html[j++]=text[i++];
+	}
+	html[j]=0;
+	return i;
+}
+
+void replaceAngleBrackets(char *text){
+	int i=0,j=0;;
+	while (text[i]){
+		switch (text[i]){
+			case '<':
+			strcpy(&html[j],"&lt;");
+			j+=4;
+			break;
+			case '>':
+			strcpy(&html[j],"&gt;");
+			j+=4;
+			break;
+			default:
+			html[j++]=text[i];
+			break;
+		}//switch
+		i++;
+	}//while
+	html[j]=0;
+}
+
+void formatNameLine(char *text,FILE *out){
+	int i,j;
+	char *start;
+    
+	i=scan(text,':',0);
+	i++;
+	i=skip(text,' ',i);
+	start=&text[i];
+	i=scan(text,' ',i+1);
+	text[i]=0;
+	replaceAngleBrackets(start);
+	fprintf(out,"<p id=\"%s\">\n",html);
+	fprintf(out,"<b>%s</b> ",html);
+	i++;
+	replaceAngleBrackets(&text[i]);
+	fputs(html,out);
+}
+
+void outputArgLine(char *line,FILE *out){
+	int i;
+	
+	line++;
+	i=word(line);
+	fprintf(out,"<div style=\"margin-left:5%%;\"><i>%s</i>&nbsp;&nbsp;",html);
+	replaceAngleBrackets(&line[i]);
+    fprintf(out,"%s</div>\n",html);
+}
+
+void addEntry(char* line, FILE *in, FILE *out){
+	char *space;
+	int i;
+	
+	// nom:
+	formatNameLine(line,out);
+	// description
+	while ((fgets(line,255,in))&&(*line==';')&&!strchr(line,':')){
+		line++;
+		replaceAngleBrackets(line);
+		fprintf(out,"<div style=\"margin-left:5%%;\">%s</div>\n",html);
+	}
+	fputs("</p>\n",out);
+		if (strstr(line,"arguments:")){
+		// arguments:
+		line++;
+		fprintf(out,"<div style=\"margin-left:5%%;\"><b>%s</b></div>\n",line);
+		while ((fgets(line,255,in))&&(*line==';')&&!strchr(line,':')){
+			outputArgLine(line,out);
+		}
+    }
+	if (strstr(line,"retourne:")){ 
+		//retourne:
+		line++;
+		fprintf(out,"<div style=\"margin-left:5%%;\"><b>%s</b></div>\n",line);
+		while ((fgets(line,255,in))&&(*line==';')){
+			outputArgLine(line,out);
+		}
+    }
+	fputs("<hr>\n",out);
+}
 
 void generateDoc(FILE *in, FILE *out){
+	char line[256];
+	
+	while (fgets(line,255,in)){
+		if (strstr(line,"; nom:")){
+			addEntry(line,in,out);
+		}
+	}
 }//generateDoc()
 
 FILE* createHeader(const char *name,FILE *out){
@@ -31,15 +144,15 @@ FILE* createHeader(const char *name,FILE *out){
 	strcpy(line,name);
 	strcat(line,html);
 	out=fopen(line,"w");
-	fputs("<DOCTYPE! HTML>\n",out);
-	fputs("<HEAD>\n",out);
-	fprintf(out,"<META NAME=\"ForthExDoc\" VALUE=\"%s\">\n",name);
-	fputs("</HEAD>\n<BODY>",out);
+	fputs("<DOCTYPE! html>\n",out);
+	fputs("<html lang=\"fr-CA\">\n",out);
+	fputs("<head>\n",out);
+	fputs("</head>\n<body>\n",out);
 	return out;
 }
 
 void closeHtml(FILE* out){
-	fputs("\n</BODY>\n</HTML>\n",out);
+	fputs("\n</body>\n</html>\n",out);
 	fclose(out);
 }
 	
